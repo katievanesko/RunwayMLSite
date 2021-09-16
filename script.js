6e07f14ead1ba9c7053a2b81cc2273756206f316
katievanesko <46686766+katievanesko@users.noreply.github.com>
2021-09-15 21:57:57 -0500

    all animal images added

--- script.js
+++ script.js
@@ -22,18 +22,6 @@ function random_text_size(min, max) {
 
 random_text_size();
 
-// function random_img_size(id) {
-//   var sideLen = Math.floor(Math.radom()*30);
-//   sideLen += "px";
-//   console.log(sideLen)
-//   document.getElementById(id).style.width = sideLen;
-//   document.getElementById(id).style.length = sideLen;
-// }
-
-// random_img_size("original");
-// random_img_size("parsed");
-// random_img_size("result");
-
 // function random_speed() {
 //     var imgIds = ["original", "parsed", "result"];
 //     var idx = 0;
@@ -61,7 +49,6 @@ random_text_size();
 //         }
 //     }
 // }
-
 // random_speed();
 
 
@@ -71,28 +58,66 @@ var pigs = ["pig_1.jpeg", "pig_2.png", "pig_3.png"];
 var monkies = ["monkey_1.jpeg", "monkey_2.png", "monkey_3.png"];
 var lions = ["lion_1.jpeg", "lion_2.png", "lion_3.png"];
 var cows = ["cow_1.jpeg", "cow_2.png", "cow_3.png"];
+var giraffes = ["giraffe_1.png", "giraffe_2.png", "giraffe_3.png"];
+var foxes = ["fox_1.png", "fox_2.png", "fox_3.png"];
+var ferrets = ["ferret_1.png", "ferret_2.png", "ferret_3.png"];
+var frogs = ["frog_1.png", "frog_2.png", "frog_3.png"];
+var fishies = ["fish_1.png", "fish_2.png", "fish_3.png"];
+var dolphins = ["dolphin_1.png", "dolphin_2.png", "dolphin_3.png"];
+var toucans = ["toucan_1.png", "toucan_2.png", "toucan_3.png"];
+var raccoons = ["raccoon_1.png", "raccoon_2.png", "raccoon_3.png"];
+var lemurs = ["lemur_1.png", "lemur_2.png", "lemur_3.png"];
+var bears = ["bear_1.png", "bear_2.png", "bear_3.png"];
+var cheetahs = ["cheetah_1.png", "cheetah_2.png", "cheetah_3.png"];
+var pandas = ["panda_1.png", "panda_2.png", "panda_3.png"];
+var sloths = ["sloth_1.png", "sloth_2.png", "sloth_3.png"];
+var owls = ["owl_1.png", "owl_2.png", "owl_3.png"];
+var snakes = ["snake_1.png", "snake_2.png", "snake_3.png"];
+var cats = ["benji_1.png", "benji_2.png", "benji_3.png"];
+var mice = ["mouse_1.png", "mouse_2.png", "mouse_3.png"];
+var doggos = ["porter_1.png", "porter_2.png", "porter_3.png"];
+var sheep = ["sheep_1.png", "sheep_2.png", "sheep_3.png"];
+var hummingbirds = ["hummingbird_1.png", "hummingbird_2.png", "hummingbird_3.png"];
 
 animalSets.push(rodents);
 animalSets.push(pigs);
 animalSets.push(monkies);
 animalSets.push(lions);
 animalSets.push(cows);
+animalSets.push(giraffes);
+animalSets.push(foxes);
+animalSets.push(ferrets);
+animalSets.push(frogs);
+animalSets.push(fishies);
+animalSets.push(dolphins);
+animalSets.push(toucans);
+animalSets.push(raccoons);
+animalSets.push(lemurs);
+animalSets.push(bears);
+animalSets.push(cheetahs);
+animalSets.push(pandas);
+animalSets.push(sloths);
+animalSets.push(owls);
+animalSets.push(snakes);
+animalSets.push(cats);
+animalSets.push(mice);
+animalSets.push(doggos);
+animalSets.push(sheep);
+animalSets.push(hummingbirds);
+
 
 document.getElementById("refresh_btn").addEventListener("click", updatePics);
 
-
-var idx = 0;
 function updatePics() {
+    idx = Math.floor(Math.random() * animalSets.length);
     var animalSet = animalSets[idx];
     document.getElementById("original").setAttribute('src', 'images/'.concat(animalSet[0]));
     document.getElementById("parsed").setAttribute('src', 'images/'.concat(animalSet[1]));
     document.getElementById("result").setAttribute('src', 'images/'.concat(animalSet[2]));
-
-    if (idx < animalSets.length - 1) {
-        idx += 1;
-    }
-    else {
-        idx = 0;
-    }
 }
 
+
+
+//variables:
+//size -- small to large or large to small
+//speed
